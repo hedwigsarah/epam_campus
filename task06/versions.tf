b9f836aadaf6690538a21c8d7277c58f774d3b43
hedwigsarah <hedwig.sarah@gmail.com>
2026-02-15 20:19:36 +0200

    Fixed typo

--- task06/versions.tf
+++ task06/versions.tf
@@ -6,7 +6,7 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.110.0, < 4.0.0"
     }
-        random = {
+    random = {
       source  = "hashicorp/random"
       version = ">= 3.0.0"
     }
