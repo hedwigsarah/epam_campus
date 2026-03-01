2532b0e4a1798d655ef6d0fe03ae286592532951
hedwigsarah <hedwig.sarah@gmail.com>
2026-03-01 21:10:29 +0200

    Fixed file structure

--- task08_b/locals.tf
+++ task08_b/locals.tf
@@ -1,14 +1,14 @@
 locals {
-  rg_name        = "${var.name_prefix}-rg"
-  aca_name       = "${var.name_prefix}-ca"
-  aca_env_name   = "${var.name_prefix}-cae"
-  acr_name       = "${replace(var.name_prefix, "-", "")}cr"
-  aks_name       = "${var.name_prefix}-aks"
-  keyvault_name  = "${var.name_prefix}-kv"
+  rg_name       = "${var.name_prefix}-rg"
+  aca_name      = "${var.name_prefix}-ca"
+  aca_env_name  = "${var.name_prefix}-cae"
+  acr_name      = "${replace(var.name_prefix, "-", "")}cr"
+  aks_name      = "${var.name_prefix}-aks"
+  keyvault_name = "${var.name_prefix}-kv"
   redis_aci_name = "${var.name_prefix}-redis-ci"
-  sa_name        = replace("${var.name_prefix}sa", "-", "")
-
-  image_name = "${var.name_prefix}-app"
-
-  common_tags = var.tags
+  sa_name       = replace("${var.name_prefix}sa", "-", "")
+  
+  image_name    = "${var.name_prefix}-app"
+  
+  common_tags   = var.tags
 }
