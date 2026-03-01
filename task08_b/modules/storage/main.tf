f1f650af14c4708b13acce8904721a496d541f07
hedwigsarah <hedwig.sarah@gmail.com>
2026-03-01 21:48:44 +0200

    fix validation errors

--- task08_b/modules/storage/main.tf
+++ task08_b/modules/storage/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_storage_account" "this" {
 
 resource "azurerm_storage_container" "this" {
   name                  = var.container_name
-  storage_account_id    = azurerm_storage_account.this.id
+  storage_account_name  = azurerm_storage_account.this.name
   container_access_type = var.container_access_type
 }
 
